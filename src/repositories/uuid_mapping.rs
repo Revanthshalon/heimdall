e0af41d553c6c329a471ea35697f6b53b9619980
Revanth Shalon Raj <revanth@Revanths-MacBook-Air.local>
2025-03-29 17:08:04 +0530

    fix: uuid mapping repository
    - updating the existing method to insert in chunks
    - updated the readonly function to be local function call

--- src/repositories/uuid_mapping.rs
+++ src/repositories/uuid_mapping.rs
@@ -3,7 +3,10 @@
 use sqlx::{Pool, Postgres};
 use uuid::Uuid;
 
-use crate::errors::{HeimdallError, HeimdallResult};
+use crate::{
+    entities::uuid_mapping::UuidMappings,
+    errors::{HeimdallError, HeimdallResult},
+};
 
 pub struct UuidMappingRepository {
     pool: Pool<Postgres>,
@@ -17,72 +20,87 @@ impl UuidMappingRepository {
 
 #[async_trait::async_trait]
 pub trait UuidMappingRepositoryTrait: Send + Sync {
-    async fn map_string_to_uuids(&self, strings: &[String]) -> HeimdallResult<Vec<Uuid>>;
-    async fn map_string_to_uuids_readonly(&self, strings: &[String]) -> HeimdallResult<Vec<Uuid>>;
+    async fn map_string_to_uuids(
+        &self,
+        nid: &Uuid,
+        strings: &[String],
+    ) -> HeimdallResult<Vec<Uuid>>;
+    async fn map_string_to_uuids_readonly(
+        &self,
+        nid: &Uuid,
+        strings: &[String],
+    ) -> HeimdallResult<Vec<Uuid>>;
     async fn map_uuid_to_strings(&self, uuids: &[Uuid]) -> HeimdallResult<Vec<String>>;
+}
+
+impl UuidMappingRepository {
+    pub fn table_name(&self) -> String {
+        String::from("heimdall_uuid_mappings")
+    }
+}
+
+impl UuidMappingRepository {
+    pub async fn batch_from_uuids(&self, uuids: &[Uuid]) -> HeimdallResult<Vec<String>> {
+        if uuids.is_empty() {
+            return Ok(Vec::new());
+        }
+        todo!()
+    }
 }
 
 #[async_trait::async_trait]
 impl UuidMappingRepositoryTrait for UuidMappingRepository {
     // This function only looks for existing mappings of strings to uuids and creates uuids for
     // mappings that does not exist
-    async fn map_string_to_uuids(&self, strings: &[String]) -> HeimdallResult<Vec<Uuid>> {
+    async fn map_string_to_uuids(
+        &self,
+        nid: &Uuid,
+        strings: &[String],
+    ) -> HeimdallResult<Vec<Uuid>> {
         if strings.is_empty() {
             return Ok(Vec::new());
         }
 
-        let mut results = Vec::with_capacity(strings.len());
+        let uuids = self.map_string_to_uuids_readonly(nid, strings).await?;
 
-        for string in strings {
-            let query_result_option: Option<Uuid> = sqlx::query_scalar(
-                "SELECT id FROM heimdall_uuid_mappings WHERE string_representation = $1",
-            )
-            .bind(string)
-            .fetch_optional(&self.pool)
-            .await?;
+        let mut mappings = Vec::with_capacity(strings.len());
 
-            let id = if let Some(id) = query_result_option {
-                id
-            } else {
-                let new_id = Uuid::new_v4();
-                sqlx::query(
-                    "INSERT INTO heimdall_uuid_mappings (id, string_representation)
-                     VALUES ($1, $2)",
-                )
-                .bind(new_id)
-                .bind(string)
-                .execute(&self.pool)
-                .await?;
-                new_id
-            };
-
-            results.push(id);
+        for (string_representation, id) in strings.iter().zip(uuids.iter()) {
+            mappings.push(UuidMappings {
+                id: *id,
+                string_representation: string_representation.clone(),
+            });
         }
-        Ok(results)
+
+        mappings.sort_by(|a, b| a.id.cmp(&b.id));
+        mappings.dedup_by(|a, b| a.id == b.id);
+
+        let chunk_size = 1000;
+        for chunk in mappings.chunks(chunk_size) {
+            let (query_str, args) = build_insert_uuids(chunk);
+            let mut query = sqlx::query(&query_str);
+
+            for arg in args {
+                query = query.bind(arg);
+            }
+
+            query.execute(&self.pool).await?;
+        }
+
+        Ok(uuids)
     }
 
-    // This function only looks for existing mappings of strings to uuids and returns an error of
-    // not present
-    async fn map_string_to_uuids_readonly(&self, strings: &[String]) -> HeimdallResult<Vec<Uuid>> {
-        if strings.is_empty() {
-            return Ok(Vec::new());
-        }
-
-        let mut results = Vec::with_capacity(strings.len());
-
-        for string in strings {
-            let query_result_option: Option<Uuid> = sqlx::query_scalar(
-                "SELECT id FROM heimdall_uuid_mappings WHERE string_representation = $1",
-            )
-            .bind(string)
-            .fetch_optional(&self.pool)
-            .await?;
-            match query_result_option {
-                Some(id) => results.push(id),
-                None => return Err(HeimdallError::NoUuidForString(string.to_string())),
-            }
-        }
-        Ok(results)
+    // NOTE: This function does not make calls to database
+    async fn map_string_to_uuids_readonly(
+        &self,
+        nid: &Uuid,
+        strings: &[String],
+    ) -> HeimdallResult<Vec<Uuid>> {
+        let result = strings
+            .iter()
+            .map(|s| Uuid::new_v5(nid, s.as_bytes()))
+            .collect();
+        Ok(result)
     }
 
     // This function looks for existing mappings of uuids to string representations and returns an
@@ -109,4 +127,30 @@ impl UuidMappingRepositoryTrait for UuidMappingRepository {
         }
         Ok(results)
     }
+}
+
+fn build_insert_uuids(uuid_mappings: &[UuidMappings]) -> (String, Vec<String>) {
+    if uuid_mappings.is_empty() {
+        return (String::new(), Vec::new());
+    }
+
+    let mut query_builder = String::new();
+    let mut args: Vec<String> = Vec::with_capacity(uuid_mappings.len() * 2);
+
+    query_builder
+        .push_str("INSERT INTO heimdall_uuid_mappings (id, string_representation) VALUES ");
+
+    for (i, uuid_mapping) in uuid_mappings.iter().enumerate() {
+        if i > 0 {
+            query_builder.push_str(", ");
+        }
+        let param_placeholder = format!("(${}, ${})", (i * 2) + 1, (i * 2) + 2);
+        query_builder.push_str(&param_placeholder);
+        args.push(uuid_mapping.id.to_string());
+        args.push(uuid_mapping.string_representation.clone())
+    }
+
+    query_builder.push_str("ON CONFLICT (id) DO NOTHING");
+
+    (query_builder, args)
 }
