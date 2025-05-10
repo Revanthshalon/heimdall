4fe1e93e2aaabad46508404288506575624cf67d
Revanth <revanth@Revanths-MacBook-Air.local>
2025-05-10 11:56:26 +0530

    update: new tests

--- src/schema/lexer/lexer_test.rs
+++ src/schema/lexer/lexer_test.rs
@@ -9,3 +9,42 @@ fn test_whitespace() {
 
     assert_eq!("Hello World", *rest.fragment())
 }
+
+#[test]
+fn test_keyword() {
+    let lex = Lexer::new("");
+    let input = Span::new("class");
+    let (_, token) = lex.lex_keyword(input).unwrap();
+
+    assert!(token.kind.eq(&TokenKind::KeywordClass));
+
+    let input = Span::new("implements");
+    let (_, token) = lex.lex_keyword(input).unwrap();
+
+    assert!(token.kind.eq(&TokenKind::KeywordImplements));
+
+    let input = Span::new("related");
+    let (_, token) = lex.lex_keyword(input).unwrap();
+
+    assert!(token.kind.eq(&TokenKind::KeywordRelated));
+
+    let input = Span::new("permits");
+    let (_, token) = lex.lex_keyword(input).unwrap();
+
+    assert!(token.kind.eq(&TokenKind::KeywordPermits));
+
+    let input = Span::new("this");
+    let (_, token) = lex.lex_keyword(input).unwrap();
+
+    assert!(token.kind.eq(&TokenKind::KeywordThis));
+
+    let input = Span::new("ctx");
+    let (_, token) = lex.lex_keyword(input).unwrap();
+
+    assert!(token.kind.eq(&TokenKind::KeywordCtx));
+
+    let input = Span::new("Namespace");
+    let (_, token) = lex.lex_keyword(input).unwrap();
+
+    assert!(token.kind.eq(&TokenKind::KeywordNamespace));
+}
