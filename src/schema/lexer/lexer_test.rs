3bad0faae6f1fb3de189983efdd6d2559005cb55
Revanth <revanth@Revanths-MacBook-Air.local>
2025-05-10 08:53:18 +0530

    test: lexer module

--- src/schema/lexer/lexer_test.rs
+++ src/schema/lexer/lexer_test.rs
@@ -1,1 +1,11 @@
+#[cfg(test)]
+use super::*;
 
+#[test]
+fn test_whitespace() {
+    let lex = Lexer::new("");
+    let input = Span::new("   Hello World");
+    let (rest, _) = lex.skip_whitespace(input).unwrap();
+
+    assert_eq!("Hello World", *rest.fragment())
+}
