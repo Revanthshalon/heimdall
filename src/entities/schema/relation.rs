52e6e5e7145fa7ff52435fcd1fddc044466438bd
Revanth <revanth@Revanths-MacBook-Air.local>
2025-05-11 20:08:21 +0530

    fix: Relation Type
    - Changed RelationType to Enum
    - Added variant Attribute type

--- src/entities/schema/relation.rs
+++ src/entities/schema/relation.rs
@@ -5,12 +5,21 @@ use super::subject::SubjectSetRewrite;
 #[derive(Debug, Clone)]
 pub struct Relation {
     pub name: Arc<str>,
-    pub relation_type: Vec<RelationType>,
+    pub relation_type: Arc<Vec<RelationType>>,
     pub subject_set_rewrite: Option<Arc<SubjectSetRewrite>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct RelationType {
-    pub namespace: Arc<str>,
-    pub relation: Option<Arc<str>>,
+pub enum RelationType {
+    Reference {
+        namespace: Arc<str>,
+        relation: Option<Arc<str>>,
+    },
+    Attribute(AttributeType),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum AttributeType {
+    Boolean,
+    String,
 }
